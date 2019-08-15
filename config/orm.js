f3f5272944577e85430ee4bab21a767175023da7
Mark B <mark.bullard42@gmail.com>
2019-08-15 13:27:35 -0400

    added code for ORM file

---
+++ config/orm.js
@@ -0,0 +1,97 @@
+// import for mysql
+var connection = require("./connection");
+
+// function for SQL syntax
+function printQuestionsMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+// convert object key/value pairs to SQL Syntax
+function toSQL(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    var value = ob[key];
+    if ((Object.hasOwnProperty, call(ob, key))) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString;
+}
+// displays all items in the database
+var orm = {
+  all: function(table, cb) {
+    var queryString = "SELECT * FROM" + table;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  //add a item(burger) to the database
+  addOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionsMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  // set "devoured" status to true.
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+    queryString += " SET ";
+    queryString += objColVals(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  // removed burger from the database
+  deleteOne: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
+
+//export ORM
+module.exports = orm;
